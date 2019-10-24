da77ce9ba078c6f8b0a09cbc5d759cb9e96ea163
Dariusz Okonski <darek200180@gmail.com>
2019-10-24 09:08:06 +0100

    Updates

---
+++ precache-manifest.84308e8e25f7c272ab1f257d727c2e65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ef05759e3625c3e5b15793d69ffcd8",
+    "url": "/favnote/index.html"
+  },
+  {
+    "revision": "6db4d36822bf559073e1",
+    "url": "/favnote/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fa19afc1c5d359308bb9",
+    "url": "/favnote/static/js/2.15b16ebb.chunk.js"
+  },
+  {
+    "revision": "6db4d36822bf559073e1",
+    "url": "/favnote/static/js/main.7f6c401c.chunk.js"
+  },
+  {
+    "revision": "585db56c0652a68989f6",
+    "url": "/favnote/static/js/runtime-main.a115827a.js"
+  }
+]);
