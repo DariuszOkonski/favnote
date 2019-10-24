69588e77423d15d9e2ec10089e953165b5716afd
Dariusz Okonski <darek200180@gmail.com>
2019-10-24 10:11:15 +0100

    add classes to elements at ListItem component

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './ListItem.css';
+import danAbramovImage from '../../../assets/images/danabramov.jpg';
 
 const ListItem = () => {
   return (
-    <li className='listItemWrapper'>
-      item1
+    <li className='listItem__wrapper'>
+      <img
+        src={danAbramovImage}
+        alt=""
+        className="listItem__image"
+      />
+      <div>
+        <h2 className="listItem__name">Dan Abramow</h2>
+        <p className="listItem__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo labore temporibus quaerat laudantium eius voluptate a error dolor. Sit aliquid cum iure aspernatur. Modi delectus enim dolores quam dignissimos veniam.</p>
+        <button className="listItem__button">visit twitter page</button>
+      </div>
     </li>
   );
 }
