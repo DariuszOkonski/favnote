d006e6fe3346a67846762841025f961d350876cb
Dariusz Okonski <darek200180@gmail.com>
2019-10-24 10:29:01 +0100

    send props to component ListItem from ListWrapper

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './ListItem.css';
-import danAbramovImage from '../../../assets/images/danabramov.jpg';
 
-const ListItem = () => {
+const ListItem = ({ image, name, description, twitterLink }) => {
   return (
     <li className='listItem__wrapper'>
       <img
-        src={danAbramovImage}
+        src={image}
         alt=""
         className="listItem__image"
       />
       <div>
-        <h2 className="listItem__name">Dan Abramow</h2>
-        <p className="listItem__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo labore temporibus quaerat laudantium eius voluptate a error dolor. Sit aliquid cum iure aspernatur. Modi delectus enim dolores quam dignissimos veniam.</p>
+        <h2 className="listItem__name">{name || 'Dariusz'}</h2>
+        <p className="listItem__description">
+          {description}
+        </p>
         <button className="listItem__button">visit twitter page</button>
       </div>
     </li>
