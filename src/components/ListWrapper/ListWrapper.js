db6a3dd95574a21fc434a9b003e58d47d767e1b0
Dariusz Okonski <darek200180@gmail.com>
2019-10-24 10:30:10 +0100

    map array twtiterAcconunts

--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ListItem from './ListItem/ListItem';
+import './ListWrapper.css';
+import { twitterAccounts } from '../../data/twitterAccounts';
 
 
 const ListWrapper = () => {
   return (
-    <ul>
-      <ListItem />
+    <ul className="listWrapper__wrapper">
+
+      {twitterAccounts.map(item =>
+
+        <ListItem
+          key={item.name}
+          {...item}
+        />
+
+      )}
+
     </ul>
   );
 }
