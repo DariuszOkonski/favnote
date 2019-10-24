bc21c2cbd0d78b7ac807d76e71f2aa36562cbab5
Dariusz Okonski <darek200180@gmail.com>
2019-10-24 09:56:52 +0100

    display ListWrapper component  at App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './index.css';
+import ListWrapper from './components/ListWrapper/ListWrapper';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>
+        <ListWrapper />
       </header>
     </div>
   );
