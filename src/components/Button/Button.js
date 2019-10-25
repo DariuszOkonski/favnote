b6d9c69db02b48e8f635bd9c862e341c384d533e
Dariusz Okonski <darek200180@gmail.com>
2019-10-25 10:18:24 +0100

    add props secondeary to component Button and write down logic

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,28 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from '../css/Button.module.css';
 
-const Button = ({ children, href }) => (
-  <>
-    {
-      href ?
-        (
-          <a
-            href={href}
-            target='_blank'
-            rel='noopener noreferrer'
-            className={styles.button}
-          >{children}</a>
-        ) : (
-          <button className={styles.button} type="submit">{children}</button>
-        )
-    }
-  </>
-);
+const Button = ({ children, href, secondary }) => {
+  const buttonClass = secondary ? styles.secondary : styles.button;
 
+  return (
+    <>
+      {
+        href ?
+          (
+            <a
+              href={href}
+              target='_blank'
+              rel='noopener noreferrer'
+              className={buttonClass}
+            >{children}</a>
+          ) : (
+            <button
+              className={buttonClass}
+              type="submit"
+            >{children}</button>
+          )
+      }
+    </>
+  )
+};
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
   href: PropTypes.string,
+  secondary: PropTypes.bool,
+}
+
+Button.defaultProps = {
+  secondary: false
 }
 
 export default Button;
