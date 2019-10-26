58b81825c620ea1755abfebd8f6cd2bb0e61f1fa
Dariusz Okonski <darek200180@gmail.com>
2019-10-26 11:13:11 +0100

    add some description to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './views/Root/Root';
+//Some description
 
 ReactDOM.render(<Root />, document.getElementById('root'));
