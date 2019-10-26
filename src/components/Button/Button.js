8b05242a8e9f731648f8380a4d9d27961e5accaa
Dariusz Okonski <darek200180@gmail.com>
2019-10-26 08:52:26 +0100

    send props with closing modal to component Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from '../css/Button.module.css';
 
-const Button = ({ children, href, secondary }) => {
+const Button = ({ children, href, secondary, ...props }) => {
   const buttonClass = secondary ? styles.secondary : styles.button;
 
   return (
@@ -18,6 +18,7 @@ const Button = ({ children, href, secondary }) => {
             >{children}</a>
           ) : (
             <button
+              {...props}
               className={buttonClass}
               type="submit"
             >{children}</button>
