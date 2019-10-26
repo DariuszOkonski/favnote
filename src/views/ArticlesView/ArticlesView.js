db5cf50a3d4cb81237cf972919a28264ec62d5bb
Dariusz Okonski <darek200180@gmail.com>
2019-10-26 19:29:13 +0100

    add AppContext and List component

--- src/views/ArticlesView/ArticlesView.js
+++ src/views/ArticlesView/ArticlesView.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import AppContext from '../../context';
+import List from '../../components/ListWrapper/ListWrapper';
 
 const ArticlesView = () => {
   return (
-    <p>This is Articles View</p>
+    <AppContext.Consumer>
+      {(context) => (
+        <List items={context.article} />
+      )}
+    </AppContext.Consumer>
   );
 }
 
