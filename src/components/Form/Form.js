924a9f73090fa9696b7b25b22e80ae58233bc38d
Dariusz Okonski <darek200180@gmail.com>
2019-10-26 12:02:16 +0100

    add AppContext to component Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AppContext from '../../context';
 import PropTypes from 'prop-types';
 import styles from '../css/Form.module.css';
 import Input from '../Input/Input';
@@ -32,76 +33,81 @@ class Form extends Component {
     const { activeOption } = this.state;
 
     return (
-      <div className={styles.wrapper}>
-        <Title>Add new {descriptions[activeOption]}</Title>
 
-        <form
-          className={styles.form__form}
-          // onSubmit={(e) => this.props.submitFn(e)}
-          onSubmit={this.props.submitFn}
-        >
+      <AppContext.Consumer>
+        {(context) => (
+          <div className={styles.wrapper}>
+            <Title>Add new {descriptions[activeOption]}</Title>
 
-          <div className={styles.inputWrapper}>
-            <label className={styles.lableRadio} htmlFor={types.twitter}>
-              Twitter
+            <form
+              className={styles.form__form}
+              onSubmit={context.addItem}
+            >
+
+              <div className={styles.inputWrapper}>
+                <label className={styles.lableRadio} htmlFor={types.twitter}>
+                  Twitter
               <input className={styles.inputRadio}
-                id={types.twitter}
-                type="radio"
-                checked={activeOption === types.twitter}
-                onChange={() => this.handleRadioButtonChange(types.twitter)}
-              />
-            </label>
+                    id={types.twitter}
+                    type="radio"
+                    checked={activeOption === types.twitter}
+                    onChange={() => this.handleRadioButtonChange(types.twitter)}
+                  />
+                </label>
 
-            <label className={styles.lableRadio} htmlFor={types.article}>
-              Article
+                <label className={styles.lableRadio} htmlFor={types.article}>
+                  Article
               <input className={styles.inputRadio}
-                id={types.article}
-                type="radio"
-                checked={activeOption === types.article}
-                onChange={() => this.handleRadioButtonChange(types.article)}
-              />
-            </label>
+                    id={types.article}
+                    type="radio"
+                    checked={activeOption === types.article}
+                    onChange={() => this.handleRadioButtonChange(types.article)}
+                  />
+                </label>
 
-            <label className={styles.lableRadio} htmlFor={types.note}>
-              Notes
+                <label className={styles.lableRadio} htmlFor={types.note}>
+                  Notes
               <input className={styles.inputRadio}
-                id={types.note}
-                type="radio"
-                checked={activeOption === types.note}
-                onChange={() => this.handleRadioButtonChange(types.note)}
+                    id={types.note}
+                    type="radio"
+                    checked={activeOption === types.note}
+                    onChange={() => this.handleRadioButtonChange(types.note)}
+                  />
+                </label>
+              </div>
+
+              <Input
+                name="name"
+                label={activeOption === types.twitter ? "Twitter Name" : "Title"}
+                maxLength={30}
               />
-            </label>
+
+              {activeOption !== types.note ?
+                <Input
+                  name="link"
+                  label={activeOption === types.twitter ? "Twitter Link" : "Link"}
+                /> : null
+              }
+
+              {(activeOption === types.twitter) ?
+                <Input
+                  name="image"
+                  label="Image"
+                /> : null
+              }
+
+              <Input
+                tag="textarea"
+                name="description"
+                label="Description"
+              />
+
+              <Button>add new item</Button>
+            </form>
           </div>
+        )}
+      </AppContext.Consumer>
 
-          <Input
-            name="name"
-            label={activeOption === types.twitter ? "Twitter Name" : "Title"}
-            maxLength={30}
-          />
-
-          {activeOption !== types.note ?
-            <Input
-              name="link"
-              label={activeOption === types.twitter ? "Twitter Link" : "Link"}
-            /> : null
-          }
-
-          {(activeOption === types.twitter) ?
-            <Input
-              name="image"
-              label="Image"
-            /> : null
-          }
-
-          <Input
-            tag="textarea"
-            name="description"
-            label="Description"
-          />
-
-          <Button>add new item</Button>
-        </form>
-      </div>
     );
   }
 }
