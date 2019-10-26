e3da1e8f6717619f40b1d91e880ff5493d629ace
Dariusz Okonski <darek200180@gmail.com>
2019-10-26 08:55:55 +0100

    add button element to component Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,9 +2,13 @@ import React from 'react';
 import styles from '../css/Modal.module.css';
 import Form from '../Form/Form'
 
-const Modal = () => {
+const Modal = ({ closeModalFn }) => {
   return (
     <div className={styles.wrapper}>
+      <button className={styles.button}
+        onClick={closeModalFn}
+      >x</button>
+
       <Form />
     </div>
   );
